b6564aa7fd212f082f0784fa5d69e4c6b5cbd880
Alexander Schedrov <alexander.schedrov@gmail.com>
2014-05-12 22:33:38 +0300

    Initial theming.

--- themes/thedevengine/config.rb
+++ themes/thedevengine/config.rb
@@ -45,7 +45,8 @@ relative_assets = true
 line_comments = (environment == :production) ? false : true
 
 # Output debugging info in development mode.
-sass_options = (environment == :production) ? {} : {:debug_info => true}
+# sass_options = (environment == :production) ? {} : {:debug_info => true}
+sass_options = {:debug_info => false}
 
 # Add the 'sass' directory itself as an import path to ease imports.
 add_import_path 'sass'
