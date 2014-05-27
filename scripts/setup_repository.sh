a9dc0b5760bfe3b51cb2112e222f8f81c2634176
Alexander Schedrov <alexander.schedrov@gmail.com>
2014-05-27 13:24:24 +0300

    Updated script.

--- scripts/setup_repository.sh
+++ scripts/setup_repository.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 cd $1
 git init
+touch test
+git add .
+git commit -m "Initial commit."
 cd $2
 git init
 git remote add origin $1
-git pull -u origin master
+git pull origin master
 cd $3
 git init
 git remote add origin $2
-git pull -u origin master
+git pull origin master
