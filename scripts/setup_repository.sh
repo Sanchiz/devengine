5235c8fd20868628c7d7982c785725952ea14f9b
Alexander Schedrov <alexander.schedrov@gmail.com>
2014-05-29 00:06:26 +0300

    Added repo with bare.

--- scripts/setup_repository.sh
+++ scripts/setup_repository.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
+
+: '
+Example of using:
+
+sh setup_repository.sh /opt/git/my_project /home/vagrant/sites/instance1development /home/vagrant/sites/instance1staging /home/vagrant/sites/instance1production
+
+/opt/git/my_project - repository without a working directory
+/home/vagrant/sites/instance1development - development repository
+/home/vagrant/sites/instance1staging - staging repository
+/home/vagrant/sites/instance1production - production repository
+'
+
+mkdir -p $1
 cd $1
+git --bare init
+cd $2
 git init
 touch test
 git add .
 git commit -m "Initial commit."
-cd $2
+git remote add origin $1
+git push origin master
+cd $3
 git init
 git remote add origin $1
 git pull origin master
-cd $3
+cd $4
 git init
-git remote add origin $2
+git remote add origin $1
 git pull origin master
